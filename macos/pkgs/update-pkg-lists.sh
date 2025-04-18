1bc4549a36d66011f21cd829c0e812edd18e5bbf
Emmanuel Oh <emmaneugene@outlook.com>
2025-04-18 23:23:04 +0800

    feat: Batch update packages
    - Include `uv` tools and ruby gems
    - Remove x86 formulae
    - Update SETUP.md
    
    Emmanuel Oh (emmaneugene@outlook.com)

--- macos/pkgs/update-pkg-lists.sh
+++ macos/pkgs/update-pkg-lists.sh
@@ -1,11 +1,19 @@
-#!/bin/zsh
-# homebrew
-brew list --cask > brew-casks.txt && brew leaves > brew-formulae.txt && arch -x86_64 /usr/local/Homebrew/bin/brew leaves > x86-brew-formulae.txt
+#!/usr/bin/env zsh
+# homebrew (arm64)
+brew tap > brew-taps.txt && brew list --cask > brew-casks.txt && brew leaves > brew-formulae.txt
+# homebrew (x86)
+if [[ -x "/usr/local/Homebrew/bin/brew" ]]; then
+  arch -x86_64 /usr/local/Homebrew/bin/brew leaves > x86-brew-formulae.txt
+fi
 # cargo
 cp ~/.cargo/.crates.toml cargo.toml
 # go
 ls ~/go/bin > go.txt
 # npm
 npm ls -g --json > npm.json
+# uv
+uv tool list > uv-tools.txt
 # pip
 pipdeptree -d 0 > pip.txt
+# ruby gems
+gem dep > gem-deps.txt
